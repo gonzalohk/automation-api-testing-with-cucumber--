822bb43857bc4d99eac652ec1ea21f9986b41bff
gonzalohk <goh.acm@gmail.com>
2020-09-30 10:05:15 -0400

    Utils and configuration was added

---
+++ src/test/java/configuration/Conf.java
@@ -0,0 +1,10 @@
+package configuration;
+
+public class Conf {
+    //Main URL
+    public static final String HOST="http://todo.ly/";
+
+    //USER:PWD -> AUTH Basic Base64
+    public static final String BASIC_AUTHENTICATION_HEADER="Authorization";
+    public static final String BASIC_AUTHENTICATION="Basic Z29uemFsb0Bnb256YWxvLmNvbToxMjM0NTY=";
+}
